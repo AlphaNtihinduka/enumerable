85f64e30ace69c2aab7d886070392f5fb3cd1354
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-07-23 18:00:58 +0300

    add MyList and enumerable

---
+++ my_enumarable.rb
@@ -0,0 +1,18 @@
+module MyEnumerable
+  def all?
+    each {|item| return false unless yield item} 
+    end
+    true
+  end
+
+  def any?
+    each { |item| return true if yield item }
+    false
+  end
+
+  def filter
+    new_array = []
+    each { |item| new_array.push(item) if yield(item) }
+    new_array
+  end
+end
